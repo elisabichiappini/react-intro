705a931decb1adca1821d82cea378b85890e9f02
elisabichiappini <elisabichiappini@gmail.com>
2026-04-22 14:22:32 +0200

    feat: javascript math

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 'use strict';
 console.log('ciao');
+
+const btn = document.getElementById('crea');
+const numberList = document.querySelector('.numbers');
+const total = document.querySelector('.total');
+
+btn.addEventListener('click', function() {
+    const randomNumber = Math.ceil(Math.random() * 99);
+    const countNumber = parseInt(total.innerText);
+    total.innerText = String(countNumber + 1);
+    numberList.innerHTML += `<div class="number">${randomNumber}</div>`;
+});
